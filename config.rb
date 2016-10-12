f527522a6adc7e2d52b57cfd75632949ecf7c98b
Kirill Ivanov <k.ivanov@speople.pro>
2016-10-12 13:57:28 +0600

    Update Gemfile, add unit, add DATABASE_ENV variables

--- config.rb
+++ config.rb
@@ -5,3 +5,12 @@ Pony.options = {
   :via => 'smtp',
   :via_options => ''
 }
+
+if ENV["DATABASE_URL"]
+  uri = URI.parse(ENV["DATABASE_URL"])
+  ENV["DATABASE_NAME"] = uri.path.tr('/', '')
+  ENV["DATABASE_USER"] = uri.user
+  ENV["DATABASE_PASSWORD"] = uri.password
+  ENV["DATABASE_HOST"] = uri.host
+  ENV["DATABASE_PORT"] = uri.host || 5432
+end
