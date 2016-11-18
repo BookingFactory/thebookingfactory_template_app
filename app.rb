4b796fcfb52795dea5e49c4b7c112aed8f129d95
Kirill Ivanov <k.ivanov@speople.pro>
2016-11-18 14:10:42 +0600

    Correct template settings

--- app.rb
+++ app.rb
@@ -29,6 +29,8 @@ end
 
 class Website < Sinatra::Base
 
+  @slug_hotel = false
+
   register Sinatra::ConfigFile
   register Sinatra::Namespace
 
@@ -151,17 +153,26 @@ class Website < Sinatra::Base
 
     ["/:page", "/:lang/:page"].each do |route|
       get route do
-        raise 'test'
         load_hotel_and_data
-        number = @data.website_rooms_dataset.where(:slug => params[:page])
-        # room = @data.website_custom_rooms_dataset.where(:slug => params[:page])
-        # page = @data.website_pages_dataset.where(:slug => params[:page])
-        # raise 'test'
-        # if (page.any? && page[])
-        # end
-        # liquid :custom_page, :locals => { :website_data => @drop }
-        number_drop = NumberDrop.new(number.find.first)
-        liquid :number, :locals => { :website_data => @drop, :number => number_drop }
+
+        number = @data.website_rooms_dataset.where("lower(slug) = ?", html_safe(params[:page]).downcase).first
+        room = @data.website_custom_rooms_dataset.where("lower(slug) = ?", html_safe(params[:page]).downcase).first
+        page = @data.website_pages_dataset.where("lower(slug) = ?", html_safe(params[:page]).downcase).first
+
+        if (page && page.published) || number || room
+          if number
+            number_drop = NumberDrop.new(number)
+            liquid :number, :locals => { :website_data => @drop, :number => number_drop }
+          elsif room
+            room_drop = RoomDrop.new(room)
+            liquid :other_room, :locals => { :website_data => @drop, :room => room_drop }
+          elsif page
+            page_drop = PageDrop.new(pages)
+            liquid :custom_page, :locals => { :website_data => @drop, :page => page_drop }
+          end
+        else
+          liquid :not_found_page
+        end
       end
     end
 
@@ -182,6 +193,7 @@ class Website < Sinatra::Base
   end
 
   namespace '/:slug' do
+
     ["", "/:lang"].each do |route|
       get route do
         pass if params[:lang] && params[:lang].length > 2
@@ -278,15 +290,27 @@ class Website < Sinatra::Base
     ["/:page", "/:lang/:page"].each do |route|
       get route do
         load_hotel_and_data
-        number = @data.website_rooms_dataset.where(:slug => params[:page])
-        # room = @data.website_custom_rooms_dataset.where(:slug => params[:page])
-        # page = @data.website_pages_dataset.where(:slug => params[:page])
-        # raise 'test'
-        # if (page.any? && page[])
-        # end
-        # liquid :custom_page, :locals => { :website_data => @drop }
-        number_drop = NumberDrop.new(number.find.first)
-        liquid :number, :locals => { :website_data => @drop, :number => number_drop }
+
+        number = @data.website_rooms_dataset.where("lower(slug) = ?", html_safe(params[:page]).downcase).first
+        room = @data.website_custom_rooms_dataset.where("lower(slug) = ?", html_safe(params[:page]).downcase).first
+        page = @data.website_pages_dataset.where("lower(slug) = ?", html_safe(params[:page]).downcase).first
+
+
+        if (page && page.published) || number || room
+          if number
+            number_drop = NumberDrop.new(number)
+            liquid :number, :locals => { :website_data => @drop, :number => number_drop }
+          elsif room
+            room_drop = RoomDrop.new(room)
+            liquid :other_room, :locals => { :website_data => @drop, :room => room_drop }
+          elsif page
+            page_drop = PageDrop.new(pages)
+            liquid :custom_page, :locals => { :website_data => @drop, :page => page_drop }
+          end
+        else
+          liquid :not_found_page
+        end
+
       end
     end
 
@@ -326,6 +350,7 @@ class Website < Sinatra::Base
     if settings.domains.split(',').include?(request.host) && params[:slug]
       @hotel = Hotel.where("lower(slug) = ?", html_safe(params[:slug]).downcase).last
       @base = "//#{request.host}/#{params[:slug]}/#{@lang}"
+      @slug_hotel = true
     else
       if request.host.split('.')[0] == "www"
         redirect_address = "#{request.scheme}://#{request.host.gsub('www.', '')}#{request.path}"
@@ -353,14 +378,14 @@ class Website < Sinatra::Base
       end
       if data[0]
         @data = data[0]
-        @template = @data.template.nil? ? '1' : @data.template
+        @template = @data.website_template.nil? ? '1' : @data.website_template.id
 
         Website.set :root, File.dirname(__FILE__)
-        Website.set :views, File.dirname(__FILE__) + "/views/#{@template}"
-        Website.set :public_folder, File.dirname(__FILE__) + "/views/#{@template}/assets"
+        Website.set :views, File.dirname(__FILE__) + "/#{@template}/views"
+        Website.set :public_folder, File.dirname(__FILE__) + "/#{@template}/assets"
         Website.set :static, true
 
-        @drop = WebsiteDataDrop.new(@data, @rand)
+        @drop = WebsiteDataDrop.new(@data, @rand, @lang)
       else
         raise Sinatra::NotFound
       end
